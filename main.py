8a2eabf39c023558f6be6bd888dd99e8b271a14b
Elio Thadhani <eliothadhani@outlook.com>
2025-04-26 21:21:44 -0700

    kinda works maybe not really

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ MASS = 1 # arbitrary unit system
 # Potential constants
 V0 = 0.01
 A = 0.02 # initial guess that we can change later
-initial_velocity = 1
+initial_velocity = 2
 
 # type indicating (x, y, z) coordinates
 Coordinate = Tuple[float, float, float]
@@ -22,7 +22,7 @@ Coordinate = Tuple[float, float, float]
 
 class ParticleSimulator:
 
-    def __init__(self, cuberoot_N: int = 2):
+    def __init__(self, cuberoot_N: int = 1):
         '''
             Initializes the particle simulation
 
@@ -36,11 +36,16 @@ class ParticleSimulator:
         spherCoord = rng.uniform([0, 0, 0], [1, np.pi, 2 * np.pi], size=(self.N, 3))
 
         # generate particles on a grid in a cube of side length 1.4 inside the sphere
-        self.posX, self.posY, self.posZ = np.meshgrid(
-            np.linspace(-0.7, 0.7, cuberoot_N),
-            np.linspace(-0.7, 0.7, cuberoot_N),
-            np.linspace(-0.7, 0.7, cuberoot_N)
-        )
+        # self.posX, self.posY, self.posZ = np.meshgrid(
+        #     np.linspace(-0.4, 0.4, cuberoot_N),
+        #     np.linspace(-0.4, 0.4, cuberoot_N),
+        #     np.linspace(-0.4, 0.4, cuberoot_N)
+        # )
+        self.posX = np.array([0])
+        self.posY = np.array([0])
+        self.posZ = np.array([0])
+
+
         self.posX = self.posX.ravel()
         self.posY = self.posY.ravel()
         self.posZ = self.posZ.ravel()
@@ -51,18 +56,22 @@ class ParticleSimulator:
             [initial_velocity, 0, 0],
             [initial_velocity, np.pi, 2 * np.pi],  size=(self.N, 3)
         ))
-        self.velX = velocities[:, 0]
-        self.velY = velocities[:, 1]
-        self.velZ = velocities[:, 2]
+        # self.velX = velocities[:, 0]
+        # self.velY = velocities[:, 1]
+        # self.velZ = velocities[:, 2]
+        self.velX = np.array([1])
+        self.velY = np.array([0])
+        self.velZ = np.array([0])
+
+
 
         # self.accel = np.zeros((3, self.N))
   
     @staticmethod
-    def checkCollisionsWithSphere(pos, vel):
+    def checkCollisionsWithSphere(pos, vel, accel):
         """Redirects particles which are outside of the unit sphere"""
         norms = np.linalg.norm(pos, axis=1)
         # indices of all particles outside the unit sphere
-        
         outside_indices = np.where(norms > 1)
 
         # outward normal vector of the sphere which we will use to find the tangent plane
@@ -72,22 +81,28 @@ class ParticleSimulator:
 
         # if nothing is outside then return
         if np.size(outside_indices) == 0:
-            return pos, vel
-
+            return pos, vel, accel
         # subtract twice the projection of the velocity onto the tangent plane of the sphere
         # this reflects the change in velocity due to elastic collision with the inside of the sphere
     #https://stackoverflow.com/questions/15616742/vectorized-way-of-calculating-row-wise-dot-product-two-matrices-with-scipy
     
     #https://stackoverflow.com/questions/68245372/how-to-multiply-each-row-in-matrix-by-its-scalar-in-numpy
-
+        #print('norm',normal_vector)
         #teleport back into sphere if outside of sphere    
         pos[outside_indices] = normal_vector
         #change the velocity from collision
-
+        
+        HOW_LONG_A_COLLISION_TAKES = 0.001
+        #print((vel[outside_indices] @ np.transpose(normal_vector)).diagonal()[:,None])
         # print(np.sum(self.vel[outside_indices] @ np.transpose(normal_vector), axis=1)[:, None])
         projected_velocity = (vel[outside_indices] @ np.transpose(normal_vector)).diagonal()[:,None] * normal_vector
-        vel[outside_indices] -= 2 * projected_velocity 
-        return pos, vel
+        if np.linalg.norm(vel) > 0.9:
+            print('vel', vel)
+            print('proj', projected_velocity)
+        vel[outside_indices] -= 2 * projected_velocity
+        #accel[outside_indices] -= 2 * projected_velocity / HOW_LONG_A_COLLISION_TAKES
+        #print('vel',projected_velocity)
+        return pos, vel, accel
         
 
     def energy(self) -> float:
@@ -139,7 +154,6 @@ class ParticleSimulator:
         x = radii[:,0]
         y = radii[:,1]
         z = radii[:,2]
-        print("xyz:", x,y,z)
         rsquare_real = x**2 + y**2 + z**2
         # find the zero radius index, which is the index of the force of the particle on itself
         # which is not a physical force and therefore will be set to zero later.
@@ -170,7 +184,7 @@ class ParticleSimulator:
             self.velY,
             self.velZ
         ))
-        data = solve_ivp(self.dU, t_span=(0,t), y0=passedVals, t_eval= np.linspace(0, t, fps * t))
+        data = solve_ivp(self.dU, t_span=(0,t), y0=passedVals, t_eval= np.linspace(0, t, fps * t), max_step = 0.001, str = 'RK23')
         
         #plot the sphere taken from matplotlib docs
         u = np.linspace(0, 2 * np.pi, 20)
@@ -215,6 +229,7 @@ class ParticleSimulator:
         ax.set_xlim((-1,1))
         ax.set_ylim((-1,1))
         ax.set_zlim((-1,1))
+        ax.set_aspect('equal')
 
         def update(frame):
             scat._offsets3d = (xp[frame], yp[frame], zp[frame])
@@ -231,10 +246,10 @@ class ParticleSimulator:
             #     ax_2d.set_xscale('log')
             return (scat, )
         
-        ani = animation.FuncAnimation(fig = fig, func = update, frames = t * fps, interval = 1000/fps)
+        ani = animation.FuncAnimation(fig = fig, func = update, frames = t * fps, interval = (1000/fps) - 1)
         # https://stackoverflow.com/questions/37146420/saving-matplotlib-animation-as-mp4
-        ani.save('Particles.mp4', writer = animation.FFMpegWriter(fps=fps))
-        # plt.show()
+        #ani.save('Particles.mp4', writer = animation.FFMpegWriter(fps=fps))
+        plt.show()
 
 
     
@@ -251,11 +266,8 @@ class ParticleSimulator:
         pos = np.array([U[:N], U[N:2*N], U[2*N:3*N]]).T
         vel = np.array([U[3*N:4*N], U[4*N:5*N], U[5*N:6*N]]).T
         
-        print("pos:", pos)
-        print("vel:", vel)
         
         # print("vel", vel)
-        pos, newVel = self.checkCollisionsWithSphere(pos, vel)
 
         # makes a matrix for each particle position, calculates all the forces 
         # on it and sums, and divides by mass to get accel
@@ -264,16 +276,18 @@ class ParticleSimulator:
             axis=1,
             arr=pos
         )
+        pos, newVel, newAccel = self.checkCollisionsWithSphere(pos, vel, newAccel)
+
 
         # re wraps the velocity and acceloration to be passed back
-        passedVals = np.zeros((self.N * 2, 3))
-        passedVals[: self.N, :] = newVel
-        passedVals[self.N : , :] = newAccel
+        newAccel = newAccel.T
+        newVel = newVel.T
         
-        return passedVals.ravel()
+
+        return np.concatenate((newVel[0], newVel[1], newVel[2], newAccel[0], newAccel[1], newAccel[2]))
 
 s = ParticleSimulator()
 #s.run()
 # s.runPre(0.01, 5)
-s.runIVP(10, 45)
+s.runIVP(2, 45)
 # %%
